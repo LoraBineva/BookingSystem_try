28a150e87bc42de1379fa293ba99790082435ffc
LoraBineva <124515927+LoraBineva@users.noreply.github.com>
2024-03-20 17:23:37 +0200

    Add files via upload

---
+++ Authenticate.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flights_Agency_App
+{
+    public class Authenticate
+    {
+        public class User
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+
+            public string ID { get; set; }
+        }
+        public class DatabaseManager
+        {
+            public static User AuthenticateUser(string username, string password) 
+            {
+                User user = RetrieveInfo(username, password);
+                return user;
+            }
+
+                private static User RetrieveInfo(string username, string password)
+            {
+
+                using (SqlConnection sql = new SqlConnection(@"Data Source=LAB109PC01\SQLEXPRESS; Initial Catalog=User_records; Integrated Security=True;"))
+                {
+                    sql.Open();
+
+                    string Query = "SELECT COUNT(1) FROM Records Where username=@Username AND password=@Password";
+                    SqlCommand cmd = new SqlCommand(Query, sql);
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.AddWithValue("@Username", username);
+                    cmd.Parameters.AddWithValue("@Password", password);
+                    int count = Convert.ToInt32(cmd.ExecuteScalar());
+
+                    if (count == 1)
+                    {
+
+                            return new User {Username = username, Password = password};
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Password or username is incorrect. Try again!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return null;
+                    }
+                    return null;
+                }
+            }
+
+        }
+    }
+}
---
+++ Log_In.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.DataFormats;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+using System.Data.SqlClient;
+using static Flights_Agency_App.Authenticate;
+
+namespace Flights_Agency_App
+{
+    public partial class Log_In : Form
+    {
+        public Log_In()
+        {
+            InitializeComponent();
+        }
+
+        private void log_in_btn_Click(object sender, EventArgs e)
+        {
+            string username_inp = username.Text;
+            string password_inp = password.Text;
+
+            if (!string.IsNullOrWhiteSpace(username_inp) && !string.IsNullOrWhiteSpace(password_inp))
+            {
+                Authenticate.User authenticatedUser = DatabaseManager.AuthenticateUser(username_inp, password_inp);
+
+                if (authenticatedUser != null)
+                {
+                    //Display the username on Form5
+                    Booking welcome = new Booking();
+                    welcome.Show();
+                    this.Hide();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter both username and password.");
+            }
+        }
+
+        private void register_btn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Register register = new Register();
+            register.ShowDialog();
+        }
+    }
+}
